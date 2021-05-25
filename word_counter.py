401b46dac4aee5c225e19ad1f5694a4ce52ec621
YashVMonpara <yashmonpara11@gmail.com>
2021-05-25 20:22:56 +0530

    Count specific words from a file

---
+++ word_counter.py
@@ -0,0 +1,14 @@
+file_path = r'text.txt'
+total_words = 0
+
+with open(file_path, 'r') as file:
+    data = file.read().lower()
+    word = 'google'
+    clean_data = data.replace('.', ' ').replace(',', ' ')
+    splitted_list = clean_data.split(' ')
+    for w in splitted_list:
+        if w == word:
+            total_words += 1
+        else:
+            pass
+    print(total_words)
